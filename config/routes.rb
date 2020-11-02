b8bda8045a505176a4f74791bf9b8f37dad93e67
mtheusbrito <matheusbritodasilva@gmail.com>
2020-11-02 00:05:56 -0300

    controllers migrations and helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/', controller: 'welcome', action: 'index'
+  resources :coins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
