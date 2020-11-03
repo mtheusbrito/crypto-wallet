a97627c85035917ab0f09df10e56dcfe48462ef3
mtheusbrito <matheusbritodasilva@gmail.com>
2020-11-02 23:35:15 -0300

    identifying development environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def date_br(data_us)
     data_us.strftime("%d/%m/%Y")
   end
+
+  def ambiente
+  if Rails.env.development?
+    "desenvolvimento"
+  elsif Rails.env.production?
+    "producao"
+  else
+    "teste"
+  end
+end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/', controller: 'welcome', action: 'index'
+  # get '/', controller: 'welcome', action: 'index'
+  root to: 'welcome#index'
   resources :coins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
