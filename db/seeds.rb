4f8dc242f2210715dfa729f22a3a381c11f551fd
mtheusbrito <matheusbritodasilva@gmail.com>
2020-11-04 02:13:46 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://i-invdn-com.akamaized.net/ico_flags/80x80/v32/ethereum.png"
+);
